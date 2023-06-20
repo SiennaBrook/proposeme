50d696332b1b6183b79e4ac82d1999da1d146da1
SiennaBrook <91650055+SiennaBrook@users.noreply.github.com>
2023-06-20 23:41:12 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,15 @@
+function youloveme(hisanswer) {
+var myanswer;
+ switch(hisanswer) {
+ case "yes":
+   myanswer ="I love you";
+   break;
+ case "no":
+    myanswer ="fuck off";
+    break;
+    default :
+    myanswer= "chup chap answer de";
+ }
+ return myanswer;
+}
+window.youloveme("")
